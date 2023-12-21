c6698378f3e635766d9532f6138dfac261941fee
JeanIzar <pierroixd@gmail.com>
2023-12-21 10:52:53 -0500

    Part 6

--- src/controllers/todo.controllers.go
+++ src/controllers/todo.controllers.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/JeanIzar/crud-go/src/config"
 	"github.com/JeanIzar/crud-go/src/models"
@@ -15,82 +14,90 @@ import (
 var db *gorm.DB = config.ConnectDB()
 
 type todoRequest struct {
-	IDCompra            uint      `json:"idcompra" gorm:"primaryKey"`
-	Compracod           int       `json:"compracod"`
-	Codigo              string    `json:"codigo" sql:"varchar(45)"`
-	Compra_fecha_emi    time.Time `json:"comprafechaemi"`
-	Compra_fecha_venc   time.Time `json:"comprafechavenc"`
-	Compra_serie        string    `json:"compraserie" sql:"varchar(45)"`
-	Compra_numer        string    `json:"compranumer" sql:"varchar(45)"`
-	Compra_Descrip      string    `json:"compradescrip" sql:"type:text"`
-	Compra_Obser        string    `json:"compraobser" sql:"type:text"`
-	Compra_total_item   int       `json:"compratotalitem"`
-	Compra_tipo_cambio  float32   `json:"compratipocambio" sql:"type:decimal(20,3)"`
-	Compra_vbb          float32   `json:"compra_vbb" sql:"type:decimal(20,2)"`
-	Compra_Vvitem       float32   `json:"compra_vvitem" sql:"type:decimal(20,2)"`
-	Compra_Dscto_global float32   `json:"compra_dsctoglobal" sql:"decimal(20,2)"`
-	Compra_Isc          float32   `json:"compra_isc" sql:"decimal(20,2)"`
-	Compra_Vv           float32   `json:"compra_vv" sql:"decimal(20,2)"`
-	Compra_Pv           float32   `json:"compra_pv" sql:"decimal(20,2)"`
-	Compra_Cargo        float32   `json:"compra_cargo" sql:"decimal(20,2)"`
-	Compra_Tot_v        float32   `json:"compra_totv" sql:"decimal(20,2)"`
-	Compra_Tot_v_mn     float32   `json:"compra_totvmn" sql:"decimal(20,2)"`
-	Compra_Oper_A       float32   `json:"compra_oa" sql:"decimal(20,2)"`
-	Compra_Oper_I       float32   `json:"compra_oi" sql:"decimal(20,2)"`
-	Compra_Oper_E       float32   `json:"compra_oe" sql:"decimal(20,2)"`
-	Compra_Cant_Pago    int       `json:"compra_cantpago"`
-	IDEstado            int       `json:"idestado"`
-	IDProveedor         int       `json:"idproveedor"`
-	ID_Tipodocumento    int       `json:"id_tipodocumento"`
-	ID_Periodo          int       `json:"id_periodo"`
-	ID_Moneda           int       `json:"id_moneda"`
-	ID_Empresa          int       `json:"id_empresa"`
-	ID_Local            int       `json:"id_local"`
-	ID_Centro_Costo     int       `json:"id_centrocosto"`
-	ID_Persona          int       `json:"id_persona"`
-	Compra_Cant_Asiento int       `json:"compra_cantasiento"`
-	Cod_Usua_Reg        int       `json:"cod_us_reg"`
-	Cod_Usua_Act        int       `json:"cod_us_act"`
+	IDCompra            uint    `json:"idcompra" gorm:"primaryKey"`
+	Compracod           int     `json:"compracod"`
+	Codigo              string  `json:"codigo" sql:"varchar(45)"`
+	Compra_fecha_emi    string  `json:"comprafechaemi" sql:"type:date"`
+	Compra_fecha_venc   string  `json:"comprafechavenc" sql:"type:date"`
+	Compra_serie        string  `json:"compraserie" sql:"varchar(45)"`
+	Compra_numer        string  `json:"compranumer" sql:"varchar(45)"`
+	Compra_Descrip      string  `json:"compradescrip" sql:"type:text"`
+	Compra_Obser        string  `json:"compraobser" sql:"type:text"`
+	Compra_total_item   int     `json:"compratotalitem"`
+	Compra_tipo_cambio  float32 `json:"compratipocambio" sql:"type:decimal(20,3)"`
+	Compra_vbb          float32 `json:"compra_vbb" sql:"type:decimal(20,2)"`
+	Compra_dscto_item   float32 `json:"compra_dscto_item" sql:"type:decimal(20,2)"`
+	Compra_Oper_G       float32 `json:"compra_og" sql:"type:decimal(20,2)"`
+	Compra_Vvitem       float32 `json:"compra_vvitem" sql:"type:decimal(20,2)"`
+	Compra_Dscto_global float32 `json:"compra_dsctoglobal" sql:"decimal(20,2)"`
+	Compra_Isc          float32 `json:"compra_isc" sql:"decimal(20,2)"`
+	Compra_Vv           float32 `json:"compra_vv" sql:"decimal(20,2)"`
+	Compra_Igv          float32 `json:"compra_igv" sql:"decimal(20,2)"`
+	Compra_Pv           float32 `json:"compra_pv" sql:"decimal(20,2)"`
+	Compra_Cargo        float32 `json:"compra_cargo" sql:"decimal(20,2)"`
+	Compra_Tot_v        float32 `json:"compra_totv" sql:"decimal(20,2)"`
+	Compra_Tot_v_mn     float32 `json:"compra_totvmn" sql:"decimal(20,2)"`
+	Compra_Oper_A       float32 `json:"compra_oa" sql:"decimal(20,2)"`
+	Compra_Oper_I       float32 `json:"compra_oi" sql:"decimal(20,2)"`
+	Compra_Oper_E       float32 `json:"compra_oe" sql:"decimal(20,2)"`
+	Compra_Cant_Pago    int     `json:"compra_cantpago"`
+	IDEstado            int     `json:"idestado"`
+	IDProveedor         int     `json:"idproveedor"`
+	ID_Tipodocumento    int     `json:"id_tipodocumento"`
+	ID_Tipocomprobante  int     `json:"id_tipocomprobante"`
+	ID_Periodo          int     `json:"id_periodo"`
+	ID_Moneda           int     `json:"id_moneda"`
+	ID_Empresa          int     `json:"id_empresa"`
+	ID_Local            int     `json:"id_local"`
+	ID_Centro_Costo     int     `json:"id_centrocosto"`
+	ID_Persona          int     `json:"id_persona"`
+	Compra_Cant_Asiento int     `json:"compra_cantasiento"`
+	Cod_Usua_Reg        int     `json:"cod_us_reg"`
+	Cod_Usua_Act        int     `json:"cod_us_act"`
 }
 
 type todoResponse struct {
 	todoRequest
-	IDCompra            uint      `json:"idcompra" gorm:"primaryKey"`
-	Compracod           int       `json:"compracod"`
-	Codigo              string    `json:"codigo" sql:"varchar(45)"`
-	Compra_fecha_emi    time.Time `json:"comprafechaemi"`
-	Compra_fecha_venc   time.Time `json:"comprafechavenc"`
-	Compra_serie        string    `json:"compraserie" sql:"varchar(45)"`
-	Compra_numer        string    `json:"compranumer" sql:"varchar(45)"`
-	Compra_Descrip      string    `json:"compradescrip" sql:"type:text"`
-	Compra_Obser        string    `json:"compraobser" sql:"type:text"`
-	Compra_total_item   int       `json:"compratotalitem"`
-	Compra_tipo_cambio  float32   `json:"compratipocambio" sql:"type:decimal(20,3)"`
-	Compra_vbb          float32   `json:"compra_vbb" sql:"type:decimal(20,2)"`
-	Compra_Vvitem       float32   `json:"compra_vvitem" sql:"type:decimal(20,2)"`
-	Compra_Dscto_global float32   `json:"compra_dsctoglobal" sql:"decimal(20,2)"`
-	Compra_Isc          float32   `json:"compra_isc" sql:"decimal(20,2)"`
-	Compra_Vv           float32   `json:"compra_vv" sql:"decimal(20,2)"`
-	Compra_Pv           float32   `json:"compra_pv" sql:"decimal(20,2)"`
-	Compra_Cargo        float32   `json:"compra_cargo" sql:"decimal(20,2)"`
-	Compra_Tot_v        float32   `json:"compra_totv" sql:"decimal(20,2)"`
-	Compra_Tot_v_mn     float32   `json:"compra_totvmn" sql:"decimal(20,2)"`
-	Compra_Oper_A       float32   `json:"compra_oa" sql:"decimal(20,2)"`
-	Compra_Oper_I       float32   `json:"compra_oi" sql:"decimal(20,2)"`
-	Compra_Oper_E       float32   `json:"compra_oe" sql:"decimal(20,2)"`
-	Compra_Cant_Pago    int       `json:"compra_cantpago"`
-	IDEstado            int       `json:"idestado"`
-	IDProveedor         int       `json:"idproveedor"`
-	ID_Tipodocumento    int       `json:"id_tipodocumento"`
-	ID_Periodo          int       `json:"id_periodo"`
-	ID_Moneda           int       `json:"id_moneda"`
-	ID_Empresa          int       `json:"id_empresa"`
-	ID_Local            int       `json:"id_local"`
-	ID_Centro_Costo     int       `json:"id_centrocosto"`
-	ID_Persona          int       `json:"id_persona"`
-	Compra_Cant_Asiento int       `json:"compra_cantasiento"`
-	Cod_Usua_Reg        int       `json:"cod_us_reg"`
-	Cod_Usua_Act        int       `json:"cod_us_act"`
+	IDCompra            uint    `json:"idcompra" gorm:"primaryKey"`
+	Compracod           int     `json:"compracod"`
+	Codigo              string  `json:"codigo" sql:"varchar(45)"`
+	Compra_fecha_emi    string  `json:"comprafechaemi" sql:"type:date"`
+	Compra_fecha_venc   string  `json:"comprafechavenc" sql:"type:date"`
+	Compra_serie        string  `json:"compraserie" sql:"varchar(45)"`
+	Compra_numer        string  `json:"compranumer" sql:"varchar(45)"`
+	Compra_Descrip      string  `json:"compradescrip" sql:"type:text"`
+	Compra_Obser        string  `json:"compraobser" sql:"type:text"`
+	Compra_total_item   int     `json:"compratotalitem"`
+	Compra_tipo_cambio  float32 `json:"compratipocambio" sql:"type:decimal(20,3)"`
+	Compra_vbb          float32 `json:"compra_vbb" sql:"type:decimal(20,2)"`
+	Compra_dscto_item   float32 `json:"compra_dscto_item" sql:"type:decimal(20,2)"`
+	Compra_Oper_G       float32 `json:"compra_og" sql:"type:decimal(20,2)"`
+	Compra_Vvitem       float32 `json:"compra_vvitem" sql:"type:decimal(20,2)"`
+	Compra_Dscto_global float32 `json:"compra_dsctoglobal" sql:"decimal(20,2)"`
+	Compra_Isc          float32 `json:"compra_isc" sql:"decimal(20,2)"`
+	Compra_Vv           float32 `json:"compra_vv" sql:"decimal(20,2)"`
+	Compra_Igv          float32 `json:"compra_igv" sql:"decimal(20,2)"`
+	Compra_Pv           float32 `json:"compra_pv" sql:"decimal(20,2)"`
+	Compra_Cargo        float32 `json:"compra_cargo" sql:"decimal(20,2)"`
+	Compra_Tot_v        float32 `json:"compra_totv" sql:"decimal(20,2)"`
+	Compra_Tot_v_mn     float32 `json:"compra_totvmn" sql:"decimal(20,2)"`
+	Compra_Oper_A       float32 `json:"compra_oa" sql:"decimal(20,2)"`
+	Compra_Oper_I       float32 `json:"compra_oi" sql:"decimal(20,2)"`
+	Compra_Oper_E       float32 `json:"compra_oe" sql:"decimal(20,2)"`
+	Compra_Cant_Pago    int     `json:"compra_cantpago"`
+	IDEstado            int     `json:"idestado"`
+	IDProveedor         int     `json:"idproveedor"`
+	ID_Tipodocumento    int     `json:"id_tipodocumento"`
+	ID_Tipocomprobante  int     `json:"id_tipocomprobante"`
+	ID_Periodo          int     `json:"id_periodo"`
+	ID_Moneda           int     `json:"id_moneda"`
+	ID_Empresa          int     `json:"id_empresa"`
+	ID_Local            int     `json:"id_local"`
+	ID_Centro_Costo     int     `json:"id_centrocosto"`
+	ID_Persona          int     `json:"id_persona"`
+	Compra_Cant_Asiento int     `json:"compra_cantasiento"`
+	Cod_Usua_Reg        int     `json:"cod_us_reg"`
+	Cod_Usua_Act        int     `json:"cod_us_act"`
 }
 
 func CreateTodo(context *gin.Context) {
@@ -114,6 +121,8 @@ func CreateTodo(context *gin.Context) {
 	todo.Compra_total_item = data.Compra_total_item
 	todo.Compra_tipo_cambio = data.Compra_tipo_cambio
 	todo.Compra_vbb = data.Compra_vbb
+	todo.Compra_dscto_item = data.Compra_dscto_item
+	todo.Compra_Oper_G = data.Compra_Oper_G
 	todo.Compra_Vvitem = data.Compra_Vvitem
 	todo.Compra_Dscto_global = data.Compra_Dscto_global
 	todo.Compra_Isc = data.Compra_Isc
@@ -158,6 +167,8 @@ func CreateTodo(context *gin.Context) {
 	response.Compra_total_item = todo.Compra_total_item
 	response.Compra_tipo_cambio = todo.Compra_tipo_cambio
 	response.Compra_vbb = todo.Compra_vbb
+	response.Compra_dscto_item = todo.Compra_dscto_item
+	response.Compra_Oper_G = todo.Compra_Oper_G
 	response.Compra_Vvitem = todo.Compra_Vvitem
 	response.Compra_Dscto_global = todo.Compra_Dscto_global
 	response.Compra_Isc = todo.Compra_Isc
@@ -242,6 +253,8 @@ func UpdateTodo(context *gin.Context) {
 	todo.Compra_total_item = data.Compra_total_item
 	todo.Compra_tipo_cambio = data.Compra_tipo_cambio
 	todo.Compra_vbb = data.Compra_vbb
+	todo.Compra_dscto_item = data.Compra_dscto_item
+	todo.Compra_Oper_G = data.Compra_Oper_G
 	todo.Compra_Vvitem = data.Compra_Vvitem
 	todo.Compra_Dscto_global = data.Compra_Dscto_global
 	todo.Compra_Isc = data.Compra_Isc
